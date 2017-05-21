3a4683bb4d0df64744e017019c258461cce97d0f
128BITHERO <dainnen@gmail.com>
2017-05-21 16:16:30 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # streamcam
+# streamcam
---
+++ startcam.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ip=$(/sbin/ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
+
+ffserver -f ~/cam.conf &
+sleep 3
+ffmpeg -f video4linux2 -i /dev/video0 http://$ip:8091/feed1.ffm 
+pkill ffserver
+rm ~/feed1.ffm
+exit 0
